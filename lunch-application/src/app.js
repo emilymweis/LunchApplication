a976db7d728b8339bf499c0d7d41c1fe296c0311
Emily Weis <eweis@chipotle.com>
2018-07-09 14:26:28 -0600

    updates for edit top five connection to POST

--- lunch-application/src/app.js
+++ lunch-application/src/app.js
@@ -4,14 +4,9 @@ import AppLayout from './theme/Layout.vue'
 import router from './router'
 
 import VueOnToast from 'vue-on-toast'
-import BootstrapVue from 'bootstrap-vue'
 import AppHeader from './theme/AppHeader.vue'
 import LoadingIndicator from './theme/LoadingIndicator.vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
 Vue.use(VueOnToast)
 
 Vue.component('appHeader-comp', AppHeader)
