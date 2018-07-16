5acb95b0983e0e41e40151cb40d29eb723800f6e
Emily Weis <emilymai90@gmail.com>
2018-07-16 13:04:13 -0600

    success to npm deploy

---
+++ lunch-application/deploy.js
@@ -0,0 +1,27 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+                user: 'LunchApplication__dev\\$LunchApplication__dev',                                                             // NOTE that this was username in 1.x
+                password: 'x5o7GtEuR59WWgtT1NPgyWQfriyNrawwwjEAw5rAhnKTmFZ1r3Q57r0l7Ccu',                                          // optional, prompted if none given
+                host: 'waws-prod-dm1-053.ftp.azurewebsites.windows.net',
+                port: 21,
+                localRoot: __dirname + '/dist',
+                remoteRoot: '/site/wwwroot/',
+                include: ['*', '**/*'],                                       // this would upload everything except dot files
+                exclude: []                                                                          // e.g. exclude sourcemaps
+//            deleteRemote: true                                                       // delete existing files at destination before uploading
+};
+
+// add console out put
+ftpDeploy.on('uploading', function(data) {
+                console.log(data.transferredFileCount + '/' + data.totalFilesCount + '\t' + data.filename);
+});
+//ftpDeploy.on('uploaded', function(data) {
+//            console.log(data);                                           // same data as uploading event
+//});
+
+// use with promises
+ftpDeploy.deploy(config)
+                .then(res => console.log('finished'))
+                .catch(err => console.log(err));
